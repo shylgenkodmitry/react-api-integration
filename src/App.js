036cec57ab87a856ebae3d4f9e272710c70bb235
Dmitry Shylgenko <shylgenko.dmitry@gmail.com>
2019-05-20 13:15:49 +0300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react'
+
+class App extends Component {
+    state = {
+        data: [],
+    };
+
+    componentDidMount() {
+        const url = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=Seona+Dancing&format=json&origin=*';
+
+        fetch(url)
+            .then(result => result.json())
+            .then(result => {
+                this.setState({
+                    data: result,
+                })
+            })
+    }
+
+    render() {
+        const { data } = this.state;
+
+        console.log(data);
+
+        const result = data.map((entry, index) => {
+            return <li key={index}>{entry}</li>
+        });
+
+        return (
+            <div className="container">
+                <ul>{result}</ul>
+            </div>
+        )
+    }
+}
+
+export default App
